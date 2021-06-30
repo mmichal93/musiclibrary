d49bfd9cea74dbbe2a74eb9626aeb0cf1fcdef8d
mmichal93 <mody_m@onet.eu>
2021-07-01 00:35:54 +0200

    Test

---
+++ src/test/java/pl/mmichal93/musiclibrary/MusiclibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mmichal93.musiclibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusiclibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
