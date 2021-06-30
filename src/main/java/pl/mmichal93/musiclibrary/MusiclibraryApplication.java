6efac97791d8612805ed0b40eb14690a027dfae7
mmichal93 <mody_m@onet.eu>
2021-07-01 00:35:18 +0200

    Initial commit

---
+++ src/main/java/pl/mmichal93/musiclibrary/MusiclibraryApplication.java
@@ -0,0 +1,13 @@
+package pl.mmichal93.musiclibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusiclibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusiclibraryApplication.class, args);
+	}
+
+}
