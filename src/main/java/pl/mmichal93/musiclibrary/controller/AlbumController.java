1a742700d64ad0cc158cdac9dbb0e0dcd8bca50d
mmichal93 <mody_m@onet.eu>
2021-07-02 08:30:33 +0200

    Swagger - small documentation of Album controller

--- src/main/java/pl/mmichal93/musiclibrary/controller/AlbumController.java
+++ src/main/java/pl/mmichal93/musiclibrary/controller/AlbumController.java
@@ -1,5 +1,6 @@
 package pl.mmichal93.musiclibrary.controller;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,30 +21,40 @@ public class AlbumController {
     }
 
     @PostMapping("/add")
+    @ApiOperation(value = "Add album",
+            notes = "Add album provided in request body",response = Album.class)
     public ResponseEntity<Album> addAlbum(@RequestBody Album album){
         Album newAlbum = albumService.addAlbum(album);
         return new ResponseEntity<>(newAlbum, HttpStatus.CREATED);
     }
 
     @GetMapping("/all")
+    @ApiOperation(value = "Find all albums",
+            notes = "Provides all albums in our database",response = Album.class)
     public ResponseEntity<List<Album>> getAllAlbums(){
         List<Album> allAlbums = albumService.findAllAlbums();
         return new ResponseEntity<>(allAlbums, HttpStatus.OK);
     }
 
     @PutMapping("/update")
+    @ApiOperation(value = "Update album",
+            notes = "Update album provided in request body",response = Album.class)
     public ResponseEntity<Album> updateAlbum(@RequestBody Album album){
         Album updatedAlbum = albumService.updateAlbum(album);
         return new ResponseEntity<>(updatedAlbum,HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/{id}")
+    @ApiOperation(value = "Delete album",
+            notes = "Delete album by providing it's id")
     public ResponseEntity<?> deleteAlbum(@PathVariable("id") Long id){
         albumService.deleteAlbum(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
+    @ApiOperation(value = "Find album by id",
+            notes = "Allows to obtain album by providing it's id",response = Album.class)
     public ResponseEntity<Album> findAlbumById(@PathVariable("id") Long id){
         Album albumById = albumService.findAlbumById(id);
         return new ResponseEntity<>(albumById,HttpStatus.OK);
